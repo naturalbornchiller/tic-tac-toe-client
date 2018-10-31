4114726efa9988d7ff31a68b18c22c193c6f193d
candydasein <michaellorensiegel@gmail.com>
2018-10-31 16:00:46 -0400

    Notes on game logic and API added to app.js
    Beginning of notes on game logic after meeting with Elizabeth
    Game board coordinates changed to match game board array on API --
    i.e., 0,1,2,3,4,5,6,7,8 rather than coordinate pairs.
    No other changes from previous commits.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -7,5 +7,20 @@
 // require('./example')
 
 $(() => {
-  // your JS code goes here
+  
 })
+
+// board is an array with single numbers as coordinates, from 0-8
+  // array in JS that captures a given current state of the board
+  // function that iterates through array to determine if there are any 3 in a rows
+  // simply map out every single possible path to victory in if statements:
+  // 0-1-2, 3-4-5, 6-7-8, 0-3-6, 1-4-7, 2-5-8, 0-4-8, 2-4-6
+  // e.g., if 0 and 1 and 2 === 'o' then 'o' wins and "over" = true then ___
+
+  // game actions come from api
+  // game states get saved in api
+  // each click stores an x or an o in a specific index (depending on the
+  // board position of click) within a string array called "cells" which lives within
+  // an object for each indidivual game played (each of which has a unique id) 
+  // all of which exists within an array called "games" 
+  // each individual game also has 
