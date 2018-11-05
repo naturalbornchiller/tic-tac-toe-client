9d947d64afb4fc7c2b1d4775c00eadaa1bec3098
candydasein <michaellorensiegel@gmail.com>
2018-11-05 17:35:02 -0500

    Fixing branch again

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,11 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-<<<<<<< HEAD
   production: 'https://aqueous-atoll-85096.herokuapp.com/',
-=======
-  production: 'https://aqueous-atoll-85096.herokuapp.com',
->>>>>>> development
   development: 'https://tic-tac-toe-wdi.herokuapp.com/'
 }
 
